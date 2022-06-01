27692f46a59ae8d7bd7d0454c827b8dae040b329
maxilipi <104212047+maxilipi@users.noreply.github.com>
2022-06-01 20:09:38 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+
+let alumno: string = prompt ("ingresar nombre del alumno")); 
+
+
+let num: number [] = new Array (3);
+let Suma : number[] = new Array(3);
+let indice: number;
+
+num[0] = Number(prompt("Ingrese el valor 1");
+num[1] = Number(prompt("Ingrese el valor 2");
+num[2] = Number(prompt("Ingrese el valor 3");
+
+indice = 0; 
+
+Suma [indice] = num[0] + num[1] + num[2]; 
+
+console.log (Suma[indice] / 3);
